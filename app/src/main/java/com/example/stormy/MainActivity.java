3568fb46b16cd76a2a563642d60c7254022c2a96
Arturo Castro <armicasdi@gmail.com>
2019-12-15 06:18:22 -0600

    Async

--- app/src/main/java/com/example/stormy/MainActivity.java
+++ app/src/main/java/com/example/stormy/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.stormy;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.io.IOException;
 
 import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Request.Builder;
@@ -33,17 +37,41 @@ public class MainActivity extends AppCompatActivity {
 
         Request request = new Request.Builder().url(forecastURl).build();
         Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
 
-        try {
-            Response response = call.execute();
-
-            if(response.isSuccessful()){
-                Log.v(TAG, response.body().string());
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-            Log.e(TAG, "IO Exception caight", e);
-        }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                try {
+                    Log.v(TAG, response.body().string());
+
+                    if(response.isSuccessful()){
+
+                    }else{
+                        alertUserAboutError();
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Log.e(TAG, "IO Exception caight", e);
+                }
+            }
+        });
+
+        Log.d(TAG, "Main Code is Running");
+
+
+
+
+
+    }
+
+    private void alertUserAboutError() {
+        AlertDialogFragment dialog = new AlertDialogFragment();
+        dialog.show(getFragmentManager(), "error_dialog");
 
     }
 }
