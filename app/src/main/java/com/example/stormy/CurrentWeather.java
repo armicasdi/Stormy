deb590dc40e21a72d38d13275b25f1f6b8ebd2d0
Arturo Castro <armicasdi@gmail.com>
2020-01-20 16:47:24 -0600

    Async

--- app/src/main/java/com/example/stormy/CurrentWeather.java
+++ app/src/main/java/com/example/stormy/CurrentWeather.java
@@ -1,5 +1,9 @@
 package com.example.stormy;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 public class CurrentWeather {
 
     private String location;
@@ -9,7 +13,29 @@ public class CurrentWeather {
     private double humidity;
     private double precipChance;
     private String summary;
+    private String timeZone;
 
+    public CurrentWeather() {
+    }
+
+    public CurrentWeather(String location, String icon, long time, double temperature, double humidity, double precipChance, String summary, String timeZone) {
+        this.location = location;
+        this.icon = icon;
+        this.time = time;
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.precipChance = precipChance;
+        this.summary = summary;
+        this.timeZone = timeZone;
+    }
+
+    public String getTimeZone() {
+        return timeZone;
+    }
+
+    public void setTimeZone(String timeZone) {
+        this.timeZone = timeZone;
+    }
 
     public String getLocation() {
         return location;
@@ -27,8 +53,60 @@ public class CurrentWeather {
         this.icon = icon;
     }
 
+    public int getIconId(){
+
+        int iconId= R.drawable.clear_day;
+
+        switch (icon){
+
+            case "clear-day":
+                iconId = R.drawable.clear_day;
+                break;
+            case "clear-night":
+                iconId= R.drawable.clear_night;
+                break;
+
+            case "rain":
+                iconId = R.drawable.rain;
+                break;
+            case "snow":
+                iconId = R.drawable.snow;
+                break;
+            case "sleet":
+                iconId = R.drawable.sleet;
+                break;
+            case "wind":
+                iconId = R.drawable.wind;
+                break;
+            case "fog":
+                iconId = R.drawable.fog;
+                break;
+            case "cloudy":
+                iconId = R.drawable.cloudy;
+                break;
+            case "partly-cloudy-day":
+                iconId = R.drawable.partly_cloudy;
+                break;
+            case "partly-cloudy-night":
+                iconId = R.drawable.cloudy_night;
+                break;
+        }
+
+        return  iconId;
+
+    }
+
+
     public long getTime() {
         return time;
+    }
+
+    public String getFormattedTime(){
+         SimpleDateFormat formater = new SimpleDateFormat("h:mm a");
+
+         formater.setTimeZone(TimeZone.getTimeZone(timeZone));
+         Date dateTime = new Date(time*1000);
+         return formater.format(dateTime);
     }
 
     public void setTime(long time) {
@@ -66,4 +144,8 @@ public class CurrentWeather {
     public void setSummary(String summary) {
         this.summary = summary;
     }
+
+
+
+
 }
--- app/src/main/java/com/example/stormy/MainActivity.java
+++ app/src/main/java/com/example/stormy/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.stormy;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.stormy.databinding.ActivityMainBinding;
 
 import org.jetbrains.annotations.NotNull;
 import org.json.JSONException;
@@ -30,15 +38,33 @@ public class MainActivity extends AppCompatActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
 
     private CurrentWeather currentWeather;
+    private ImageView iconImageView;
+    final double latitude =37.8267;
+    final double longitude =-122.4233;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        getForecast(latitude, longitude);
+
+        Log.d(TAG, "Main Code is Running");
+
+
+
+
+
+    }
+
+    private void getForecast(double latitude, double longitude ) {
+        final ActivityMainBinding binding = DataBindingUtil.setContentView(MainActivity.this, R.layout.activity_main);
+
+        TextView darkSky = findViewById(R.id.darkSkyAttribution);
+        darkSky.setMovementMethod(LinkMovementMethod.getInstance());
+
+        iconImageView = findViewById(R.id.iconImageView);
 
         String apiKey ="8c798ae8a862d546577abf4f0df5557f";
-        double latitude =37.8267;
-        double longitude =-122.4233;
+
 
         String forecastURl = "https://api.darksky.net/forecast/"
                 + apiKey +"/"+latitude +","+longitude;
@@ -47,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
 
         OkHttpClient client = new OkHttpClient();
 
-        Request request = new Request.Builder().url(forecastURl).build();
+        Request request = new Builder().url(forecastURl).build();
         Call call = client.newCall(request);
         call.enqueue(new Callback() {
             @Override
@@ -65,6 +91,27 @@ public class MainActivity extends AppCompatActivity {
                     if(response.isSuccessful()){
                         currentWeather = getCurrentDetails(jsonData);
 
+                        final CurrentWeather displayWeather = new CurrentWeather(
+                                currentWeather.getLocation(),
+                                currentWeather.getIcon(),
+                                currentWeather.getTime(),
+                                currentWeather.getTemperature(),
+                                currentWeather.getHumidity(),
+                                currentWeather.getPrecipChance(),
+                                currentWeather.getSummary(),
+                                currentWeather.getTimeZone()
+                        );
+
+                        binding.setWeather(displayWeather);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Drawable drawable = getResources().getDrawable(displayWeather.getIconId());
+                                iconImageView.setImageDrawable(drawable);
+                            }
+                        });
+
+
                     }else{
                         alertUserAboutError();
                     }
@@ -78,14 +125,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         }
-
-        Log.d(TAG, "Main Code is Running");
-
-
-
-
-
     }
+
 
     private CurrentWeather getCurrentDetails(String jsonData) throws JSONException {
 
@@ -93,7 +134,22 @@ public class MainActivity extends AppCompatActivity {
 
         String timezone = forecast.getString("timezone");
         Log.i(TAG, "from Json " + timezone);
-        return null;
+
+        JSONObject currently = forecast.getJSONObject("currently");
+        CurrentWeather currentWeather = new CurrentWeather();
+
+        currentWeather.setHumidity(currently.getDouble("humidity"));
+        currentWeather.setTime(currently.getLong("time"));
+        currentWeather.setIcon(currently.getString("icon"));
+        currentWeather.setLocation("Alcatraz Island CA");
+        currentWeather.setPrecipChance(currently.getDouble("precipProbability"));
+        currentWeather.setSummary(currently.getString("summary"));
+        currentWeather.setTemperature(currently.getDouble("temperature"));
+        currentWeather.setTimeZone(timezone);
+
+        Log.d(TAG, currentWeather.getFormattedTime());
+
+        return currentWeather;
     }
 
     private boolean isNetworkAvailable() {
@@ -114,6 +170,12 @@ public class MainActivity extends AppCompatActivity {
     private void alertUserAboutError() {
         AlertDialogFragment dialog = new AlertDialogFragment();
         dialog.show(getFragmentManager(), "error_dialog");
+
+    }
+
+    public void refreshOnClick(View view){
+        getForecast(latitude, longitude);
+        Toast.makeText(this, "Data Refreshed", Toast.LENGTH_LONG).show();
 
     }
 }
