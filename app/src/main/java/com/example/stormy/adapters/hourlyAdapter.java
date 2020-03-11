f61d502415a07e2987c0dc9bb4a55bce192265a4
Arturo Castro <armicasdi@gmail.com>
2020-03-10 23:33:30 -0600

    List Layout+

--- app/src/main/java/com/example/stormy/adapters/hourlyAdapter.java
+++ app/src/main/java/com/example/stormy/adapters/hourlyAdapter.java
@@ -1,6 +1,37 @@
 package com.example.stormy.adapters;
 
-import android.support.v7.widget.RecyclerView.Adapter;
 
-public class hourlyAdapter extends Adapter {
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class hourlyAdapter extends RecyclerView.Adapter<HourlyAdapter.ViewHolder> {
+
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        //Binding Variables
+
+        ///Construictors
+
+    }
 }
